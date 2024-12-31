c13f13765ddca9ea5c11618bb698acfb283812fd
KiloxGo <qianxianqwq@163.com>
2024-12-31 22:16:00 +0800

    add x and steam

--- src/config.ts
+++ src/config.ts
@@ -57,15 +57,15 @@ export const profileConfig: ProfileConfig = {
   links: [
     {
       name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+      icon: 'hugeicons:new-twitter',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      url: 'https://x.com/kiloxgo',
     },
     {
       name: 'Steam',
       icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
+      url: 'https://steamcommunity.com/profiles/76561199387737754',
     },
     {
       name: 'GitHub',
