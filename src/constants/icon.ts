e22669c9588b0d4fdb72d06dceab67b19c6ea954
KiloxGo <qianxianqwq@163.com>
2024-12-31 18:38:08 +0800

    fix icon

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -4,41 +4,33 @@ export const defaultFavicons: Favicon[] = [
   {
     src: '/favicon/blogicon.png',
     theme: 'light',
-    sizes: '32x32',
   },
   {
     src: '/favicon/blogicon.png',
     theme: 'light',
-    sizes: '128x128',
   },
   {
     src: '/favicon/blogicon.png',
     theme: 'light',
-    sizes: '180x180',
   },
   {
     src: '/favicon/blogicon.png',
     theme: 'light',
-    sizes: '192x192',
   },
   {
     src: '/favicon/blogicon-dark.png',
     theme: 'dark',
-    sizes: '32x32',
   },
   {
     src: '/favicon/blogicon-dark.png',
     theme: 'dark',
-    sizes: '128x128',
   },
   {
     src: '/favicon/blogicon-dark.png',
     theme: 'dark',
-    sizes: '180x180',
   },
   {
     src: '/favicon/blogicon-dark.png',
     theme: 'dark',
-    sizes: '192x192',
   },
 ]
